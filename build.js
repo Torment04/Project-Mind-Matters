5775c7fc9db60548f12c28e23a6ac4dd65ab3793
Torment <omega365alpha@gmail.com>
2025-03-27 21:34:16 +0300

    Improve routing and fix HTML links for all cases

--- build.js
+++ build.js
@@ -18,12 +18,23 @@ function fixHtmlLinks(filePath) {
   try {
     let content = fs.readFileSync(filePath, 'utf8');
     
-    // Replace relative navigation links
+    // Fix links in navigation - convert "about.html" to "/about"
     content = content.replace(/href="([^\/][^"]+)\.html"/g, 'href="/$1"');
     
-    // Fix paths to css and images
+    // Fix links that point to pages/file.html to just /file
+    content = content.replace(/href="pages\/([^"]+)\.html"/g, 'href="/$1"');
+    
+    // Fix links for images and css
     content = content.replace(/href="\.\.\/css\//g, 'href="/css/');
     content = content.replace(/src="\.\.\/images\//g, 'src="/images/');
+    
+    // Fix active link class for current page
+    const pageName = path.basename(filePath, '.html');
+    if (pageName === 'index') {
+      content = content.replace('href="/"', 'href="/" class="active"');
+    } else {
+      content = content.replace(`href="/${pageName}"`, `href="/${pageName}" class="active"`);
+    }
     
     fs.writeFileSync(filePath, content);
     console.log(`Fixed links in: ${filePath}`);
@@ -72,6 +83,22 @@ if (!fs.existsSync('public')) {
 if (fs.existsSync('index.html')) {
   copyFile('index.html', path.join('public', 'index.html'));
 }
+
+// Create a file that redirects from /pages to the root
+const pagesRedirect = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="refresh" content="0;url=/" />
+</head>
+<body>
+  Redirecting to home page...
+</body>
+</html>
+`;
+fs.mkdirSync(path.join('public', 'pages'), { recursive: true });
+fs.writeFileSync(path.join('public', 'pages', 'index.html'), pagesRedirect);
+console.log('Created redirect for /pages path');
 
 // Copy directories to public
 const dirsToCopy = ['pages', 'css', 'images'];
